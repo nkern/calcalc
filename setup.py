4b880de3ed216ff16811df8c4c2e7f79f3f5b7dc
Nicholas S. Kern <nkern@berkeley.edu>
2016-11-29 01:08:46 -0800

    new file:   setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+import sys
+try:
+    from setuptools import setup, Extension, Command
+except ImportError:
+    from distutils.core import setup, Extension, Command
+
+VERSION = "0.0.1"
+
+
+setup(
+    name="calcalc",
+    version=VERSION,
+    author="Nick Kern"
+)
+
